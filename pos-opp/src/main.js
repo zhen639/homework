696eebaa979517dca0c5c3eec503a5d8de8d6ced
zhen6939 <zhen6939@163.com>
2014-11-05 10:19:14 +0800

    add AllItemsInfo Object

--- pos-opp/src/main.js
+++ pos-opp/src/main.js
@@ -1,31 +1,8 @@
 //TODO: Please write code in this file.
-
-var itemsIndex = loadAllItems();
 var promoArray = loadPromotions();
-
-
-
-//function splitname(barc){
-//    var sname = barc.split('-');
-//    if (sname.length < 2) {
-//        sname[1] = 1;
-//    }
-//    return sname
-//}
 
 function FormatItems(inputs){
     this.content = {};
-
-//    inputs.forEach(function (barcode){
-//        var spName = splitname(barcode);
-//        var itemName = spName[0], itemCount = spName[1];
-//        if (content.hasOwnProperty(itemName)){
-//            content[itemName] += itemCount;
-//        }
-//        else{
-//            content[itemName] = 1;
-//        }
-//    })
 
     for (var i in inputs){
         var spName = splitname(inputs[i]);
@@ -38,17 +15,10 @@ function FormatItems(inputs){
         }
     }
 
-    this.splitname = function (){
-        var sname = barc.split('-');
-        if (sname.length < 2) {
-            sname[1] = 1;
-        }
-        return sname
-    }
-
     return this.content;
 }
-//
+
+
 function splitname (barc){
     var sname = barc.split('-');
     if (sname.length < 2) {
@@ -58,32 +28,33 @@ function splitname (barc){
 }
 
 
-
-function ItemBill(itemList){
-    this.bill = [];
-    itemList.forEach(function (barcode, count){
-        for (var i in itemsIndex){
-            if (itemsIndex[i].hasOwnProperty(barcode)){
-                item = {name: itemsIndex[i].name,
-                        barcode: itemsIndex[i].barcode,
-                        count: count,
-                        unit:  itemsIndex[i].unit,
-                        price:  itemsIndex[i].price,
-                        totPrice:  itemsIndex[i].price * count}
-                this.bill.push(item);
-            }
-        }
-    })
-    return this.bill;
+function AllItemsInfo(formatItemList){
+    this.purchasedItemsInfo = [];
+    for(var barc in formatItemList){
+        var itemInfo = getItemInfo(barc, formatItemList[barc]);
+        this.purchasedItemsInfo.push(itemInfo);
+    }
+    return this.purchasedItemsInfo
 }
 
-ItemBill.prototype.getDetailInfo = function (){
-    if (itemsIndex.hasOwnProperty(barcode)){
-        return 0;
+function getItemInfo(barcode, itemCount){
+    var itemsIndex = loadAllItems();
+    for(var i in itemsIndex){
+        if(itemsIndex[i].barcode == barcode){
+            return {name: itemsIndex[i].name,
+                    barcode: itemsIndex[i].barcode,
+                    unit:  itemsIndex[i].unit,
+                    price:  itemsIndex[i].price,
+                    count: itemCount,
+                    account: itemsIndex[i].price * itemCount
+                    }
+        }
     }
 }
 
 function printInventory(inputs) {
-    var itList = new FormatItems(inputs);
-    console.log(itList);
+    var itemsFormat = new FormatItems(inputs);
+//    console.log(itList);
+    var itemBill = new AllItemsInfo(itemsFormat);
+    console.log(itemBill);
 }
